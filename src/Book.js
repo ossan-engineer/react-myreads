1841180a1bfa74e85464bc110a340e0601be9276
k.tachibana <bodega1123@gmail.com>
2017-09-05 19:14:34 +0900

    feat: Use static data to render books

---
+++ src/Book.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Book.css';
+
+const Book = (props) => {
+  const {
+    id,
+    title,
+    author,
+    imageUrl,
+  } = props;
+
+  return (
+    <div className='book'>
+      <div className='book-top'>
+        <img
+          src={imageUrl}
+          className='book-cover'
+          alt={`${id}: ${title} - ${author}`}
+        />
+        <div className='book-shelf-changer'>
+          <select>
+            <option value='none' disabled>Move to...</option>
+            <option value='currentlyReading'>Currently Reading</option>
+            <option value='wantToRead'>Want to Read</option>
+            <option value='read'>Read</option>
+            <option value='none'>None</option>
+          </select>
+        </div>
+      </div>
+      <div className='book-title'>{title}</div>
+      <div className='book-authors'>{author}</div>
+    </div>
+  );
+};
+
+Book.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+};
+
+export default Book;
