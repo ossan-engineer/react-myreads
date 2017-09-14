a54105a4b84df1c87bd27803d019fbb80c51b9f2
k.tachibana <bodega1123@gmail.com>
2017-09-14 12:48:02 +0900

    feat: Update searchedBooks when shelf is changed in search page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import where from 'lodash.where';
+import find from 'lodash.find';
 import * as BooksAPI from './BooksAPI';
 import Home from './Home';
 import SearchBooks from './SearchBooks';
@@ -28,8 +29,8 @@ class App extends React.Component {
     });
   };
 
-  handleChange = (event, id) => {
-    const newMyBooks = this.state.myBooks.map(book => Object.assign({}, book));
+  handleChangeMyBook = (event, id) => {
+    const newMyBooks = this.state.myBooks.map(myBook => Object.assign({}, myBook));
     const shelf = event.target.value;
 
     newMyBooks.forEach((newMyBook) => {
@@ -43,6 +44,34 @@ class App extends React.Component {
     }, () => {
       BooksAPI.update(id, shelf);
     });
+
+    // this.updateSearchedBooks(this.state.searchedBooks);
+  };
+
+  handleChangeSearchedBook = (event, id) => {
+    // TODO: searchを表示した時は初期化する
+    const newSearchedBooks = this.state.searchedBooks
+      .map(searchedBook => Object.assign({}, searchedBook))
+      .filter(searchedBook => searchedBook.id !== id);
+    const additionalMyBook = this.state.searchedBooks.length > 0 ?
+      find(this.state.searchedBooks, {
+        id,
+      }) :
+      null;
+    const shelf = event.target.value;
+    newSearchedBooks.push(additionalMyBook);
+
+    this.setState({
+      searchedBooks: newSearchedBooks,
+    }, () => {
+      BooksAPI.update(id, shelf);
+    });
+
+    console.log('BEFORE');
+    console.log(this.state.searchedBooks);
+    // this.updateSearchedBooks(this.state.searchedBooks);
+    console.log('AFTER');
+    console.log(this.state.searchedBooks);
   };
 
   handleSearch = (event) => {
@@ -54,25 +83,30 @@ class App extends React.Component {
 
     if (this.state.query.length > 0) {
       BooksAPI.search(this.state.query).then((data) => {
-        const newSeachedBooks = data.slice(); // eslint-disable-line no-unused-vars
-
-        newSeachedBooks.forEach((searchedBook) => {
-          this.state.myBooks.forEach((myBook) => {
-            if (searchedBook.id === myBook.id) {
-              where(newSeachedBooks, { id: myBook.id })[0].shelf = myBook.shelf;
-            }
-          });
-        });
-
-        this.setState({
-          searchedBooks: Array.isArray(data) ? newSeachedBooks : [],
-        });
+        this.updateSearchedBooks(data);
       });
     } else {
       this.setState({
         searchedBooks: [],
       });
     }
+  };
+
+  updateSearchedBooks = (searchedBooks) => {
+    console.log('UPDATED');
+    const newSeachedBooks = searchedBooks.slice(); // eslint-disable-line no-unused-vars
+
+    newSeachedBooks.forEach((searchedBook) => {
+      this.state.myBooks.forEach((myBook) => {
+        if (searchedBook.id === myBook.id) {
+          where(newSeachedBooks, { id: myBook.id })[0].shelf = myBook.shelf;
+        }
+      });
+    });
+
+    this.setState({
+      searchedBooks: Array.isArray(searchedBooks) ? newSeachedBooks : [],
+    });
   };
 
   render() {
@@ -85,7 +119,7 @@ class App extends React.Component {
             render={() => (
               <Home
                 myBooks={this.state.myBooks}
-                handleChange={this.handleChange}
+                handleChange={this.handleChangeMyBook}
                 handleGetAll={this.handleGetAll}
               />
             )}
@@ -96,7 +130,7 @@ class App extends React.Component {
               <SearchBooks
                 myBooks={this.state.myBooks}
                 searchedBooks={this.state.searchedBooks}
-                handleChange={this.handleChange}
+                handleChange={this.handleChangeSearchedBook}
                 handleSearch={this.handleSearch}
                 query={this.state.query}
               />
